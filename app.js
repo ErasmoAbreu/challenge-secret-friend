86dcfd25ca9a42fbd264d6376d341dc859365b5f
ErasmoAbreu <erasmoabreu1@gmail.com>
2025-08-18 08:30:39 -0300

    Foi realizado uma nova implementação buscando deixar a programação mais defensiva para evitar possivéis erros do usuário, como a utilização de fontes minúsculas ou maiúsculas como também números, foi feito o uso de expressçoes regulares buscando deixar o código mais limpo e de fácil compreenssão.

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 let amigos = [];
 
 function adicionarAmigo() {
-    
     const inputAmigo = document.getElementById('amigo');
     const listaAmigos = document.getElementById('listaAmigos');
-    const nomeAmigo = inputAmigo.value.trim(); 
+    const nomeAmigo = inputAmigo.value.trim();
 
-    if (nomeAmigo === '') {
-        alert('Por favor, digite um nome válido.');
+ 
+    const regexNomeValido = /^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/;
+
+    if (!regexNomeValido.test(nomeAmigo)) {
+        alert('Por favor, digite um nome válido (apenas letras).');
+        inputAmigo.value = '';
+        inputAmigo.focus();
         return; 
     }
 
-    if (amigos.includes(nomeAmigo)) {
+    const nomeJaAdicionado = amigos.some(amigo => amigo.toLowerCase() === nomeAmigo.toLowerCase());
+
+    if (nomeJaAdicionado) {
         alert('Este nome já foi adicionado. Por favor, insira um nome diferente.');
-        inputAmigo.value = ''; 
-        inputAmigo.focus(); 
-        return;
+        inputAmigo.value = '';
+        inputAmigo.focus();
+        return; 
     }
 
     amigos.push(nomeAmigo);
@@ -25,22 +31,22 @@ function adicionarAmigo() {
     listaAmigos.appendChild(itemLista);
 
     inputAmigo.value = '';
-    
     inputAmigo.focus();
 }
 
+
 function sortearAmigo() {
-    
     const resultado = document.getElementById('resultado');
- 
+    
+    resultado.textContent = '';
+
     if (amigos.length < 2) {
         resultado.textContent = 'Adicione pelo menos 2 amigos para sortear!';
-        resultado.style.color = '#FF0000'; 
+        resultado.style.color = '#FF0000';
         return;
     }
 
     const indiceSorteado = Math.floor(Math.random() * amigos.length);
-
     const amigoSorteado = amigos[indiceSorteado];
 
     resultado.textContent = `O amigo secreto é: ${amigoSorteado}`;
